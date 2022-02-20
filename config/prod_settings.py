2dd395a3081dd91d390d09529dcb3359f1b00490
JJ Zolper <jzthr33@gmail.com>
2022-02-20 16:03:08 -0600

    google analytics

--- config/prod_settings.py
+++ config/prod_settings.py
@@ -9,13 +9,16 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '*')]
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-# GEO IP
+# GEO IP / Maxmind
 GEOIP_PATH=os.environ['GEOIP_GEOLITE2_PATH']
 GEOIP_CITY=os.environ['GEOIP_GEOLITE2_CITY_FILENAME']
 GEOIP_COUNTRY=os.environ['GEOIP_GEOLITE2_COUNTRY_FILENAME']
 
 MAXMIND_LICENSE_KEY=os.environ['MAXMIND_LICENSE_KEY']
+
+# Google Analytics
+GOOGLE_ANALYTICS_ID = os.getenv('GOOGLE_ANALYTICS_ID')
--- core/views.py
+++ core/views.py
@@ -10,6 +10,7 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context.update({
             'app_name': settings.APP_NAME,
+            'GOOGLE_ANALYTICS_ID': settings.GOOGLE_ANALYTICS_ID,
             'form_url': reverse('create_user')
         })
         return context
